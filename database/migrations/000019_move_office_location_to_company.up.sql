7a235b9b715ccea213e488c341ebd56178a808a1
Bima Surya <bimasurya729@gmail.com>
2026-04-17 09:40:07 +0700

    testing migration script

--- database/migrations/000019_move_office_location_to_company.up.sql
+++ database/migrations/000019_move_office_location_to_company.up.sql
@@ -2,8 +2,8 @@ ALTER TABLE companies ADD COLUMN IF NOT EXISTS office_lat DOUBLE PRECISION;
 ALTER TABLE companies ADD COLUMN IF NOT EXISTS office_long DOUBLE PRECISION;
 ALTER TABLE companies ADD COLUMN IF NOT EXISTS allowed_radius_meters INT;
 
-INSERT INTO companies (id, office_lat, office_long, allowed_radius_meters)
-SELECT DISTINCT c.id, s.office_lat, s.office_long, s.allowed_radius_meters
+INSERT INTO companies (id, name, slug, is_active, plan, max_employees, office_lat, office_long, allowed_radius_meters)
+SELECT DISTINCT c.id, c.name, c.slug, c.is_active, c.plan, c.max_employees, s.office_lat, s.office_long, s.allowed_radius_meters
 FROM companies c
 INNER JOIN schedules s ON s.company_id = c.id
 ON CONFLICT (id) DO UPDATE SET
