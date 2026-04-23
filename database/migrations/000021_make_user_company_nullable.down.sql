170b7317a078012072f9a679c2e86dc516268de8
Bima Surya <bimasurya729@gmail.com>
2026-04-23 09:51:41 +0700

    fix: migration super user

--- database/migrations/000021_make_user_company_nullable.down.sql
+++ database/migrations/000021_make_user_company_nullable.down.sql
@@ -3,3 +3,4 @@ ALTER TABLE users DROP CONSTRAINT IF EXISTS users_email_unique;
 ALTER TABLE users ALTER COLUMN company_id SET NOT NULL;
 ALTER TABLE users ADD CONSTRAINT users_company_id_fkey FOREIGN KEY (company_id) REFERENCES companies(id);
 ALTER TABLE users ADD CONSTRAINT users_company_id_email_key UNIQUE (company_id, email);
+UPDATE users SET company_id = '00000000-0000-0000-0000-000000000001' WHERE company_id IS NULL;
